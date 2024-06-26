b48c507176399d6d12089e749da6ce1ab0b7d847
babacar <babacar.diakite01@gmail.com>
2024-06-26 07:25:54 -0400

    repasotory

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
-      console.log(entry, entry.classList);
+      console.log(entry);
+    } else {
+      entry.target.classList.remove("show");
     }
   });
 });
@@ -11,28 +13,9 @@ const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 console.log(hiddenElements);
 
+/// PICTURE ANIMATION
 window.onload = function () {
   document.querySelector(".bg").style.display = "block";
   document.querySelector(".bg").style.animationName = "animate";
   document.querySelector(".imgs").style.display = "none";
 };
-
-// const scrollers = document.querySelectorAll(".scroller");
-// console.log(scrollers);
-// if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-//   addAnimation();
-// }
-
-// function addAnimation() {
-//   scrollers.forEach((scroller) => {
-//     scroller.setAttribute("data-animated", true);
-//     const scrollerInner = scroller.querySelector(".scroller-inner");
-//     const scrollerContent = Array.from(scrollerInner.children);
-//     scrollerContent.forEach((item) => {
-//       const duplicatedItems = item.cloneNode(true);
-//       console.log(duplicatedItems);
-//       duplicatedItems.setAttribute("ara-hidden", true);
-//       scrollerInner.appendChild(duplicatedItems);
-//     });
-//   });
-// }
